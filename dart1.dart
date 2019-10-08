32ab286ef0a6ffe6ebe0308c6ec13819a3ace751
krishna221097 <46162467+krishna221097@users.noreply.github.com>
2019-10-08 10:24:18 +0530

    Create dart1.dart

---
+++ dart1.dart
@@ -0,0 +1,27 @@
+void main() {
+ int score=23;
+ var count =23;
+ int hexValue=0xEADEBAEE;
+  
+  //Numbers:Double
+  double percentage=93.4;
+  var percent=82.533;
+  double exponents=1.42e5;
+  
+  //String
+  String name="Krishna";
+  var company="LTTS";
+  
+  //Boolean
+  bool isValid=true;
+  var isAlive=false;
+  
+  print("My name is $name");
+  print("The number of characters in String Kevin is ${name.length}");
+  
+  int l=20;
+  int b=10;
+  print("The sum of $l and $b is ${l+b}");
+  print("The area of rectangle with length $l and breadth $b is ${l*b}");
+  }
+
